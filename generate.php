6e392a221007c4abd596ef9f7ca681969ca15666
Krystian <krystian.gontarek@gmail.com>
2014-08-25 10:22:03 +0200

    Update generate.php

--- generate.php
+++ generate.php
@@ -3,7 +3,7 @@ $name = $_POST['sitename'];
 $gifts = $_POST['allGifts'];
 $go = $_POST['ended'];
 $file1 = fopen("gifts$name.txt", "w+");
-fputs($file1, $gifts);
+fputs($file1, "abc123abc");
 $file2 = fopen("$name.php", "w+");
 fputs($file2, "<?php readfile('szablon.html') ?>");
 ?>
