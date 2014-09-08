cd6db514234ce0282f0c91d4be4ee39081d25f53
Krystian <krystian.gontarek@gmail.com>
2014-09-08 20:00:46 +0200

    Update generate.php

--- generate.php
+++ generate.php
@@ -6,7 +6,7 @@ $ended = $_POST['ended'];
 $giftsArray = $_POST['giftsArray'];
 $resultArray = print_r($giftsArray, true);
 $file1 = fopen($giftsname, "w+");
-fputs($file1, $allGifts);
+fputs($file1, $resultArray);
 $file2 = fopen("$sitename.php", "w+");
 fputs($file1, "<link rel='stylesheet' href='css/normalize.css' />
 <link rel='stylesheet' href='css/style-grid.css' />
