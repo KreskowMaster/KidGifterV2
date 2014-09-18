a57929a74ff26e79e6a3859decc1c36223efa940
Krystian <krystian.gontarek@gmail.com>
2014-09-18 20:53:47 +0200

    Update generate.php

--- generate.php
+++ generate.php
@@ -4,13 +4,15 @@ $giftsname = "gifts.$sitename.html";
 $allGifts = $_POST['allGifts'];
 $ended = $_POST['ended'];
 $giftsArray = $_POST['giftsArray'];
-$resultArray = print_r($giftsArray, true);
 $file1 = fopen($giftsname, "w+");
-fputs($file1, $resultArray);
 $file2 = fopen("$sitename.php", "w+");
 fputs($file1, "<link rel='stylesheet' href='css/normalize.css' />
 <link rel='stylesheet' href='css/style-grid.css' />
 <link rel='stylesheet' href='css/style.css' />");
 fputs($file2, "<?php readfile('szablon.html')?>");
 fputs($file2, "<?php readfile('$giftsname')?>");
+foreach ($giftsArray as $gift) {
+    fputs($file1, $gift);
+}
+
 ?>
