232a75f7fb784258a9d2c22265ca30587d9bae4a
Krystian <krystian.gontarek@gmail.com>
2014-09-22 16:27:16 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ window.onload = function () {
     var list = new Array();
     var clear = document.getElementById("delete");
     var giftsArray = new Array();
+    var endlink = document.getElementById("endlink");
     function isEmpty(value) {
     return /^\s*$/.test(value);
 }
@@ -16,13 +17,14 @@ window.onload = function () {
         if (!isEmpty(formtitle.value)) {
         var giftinfo = "<div class='gifttext'>";
         giftinfo += "<img class='GiftImage omega' src='" + formimage.value + "'>";
-        giftinfo += "<p class='GiftTitle'>" + formtitle.value + "</p>";
-        giftinfo += "<p class='GiftDescription'>" + formdescription.value + "</p>";
+        giftinfo += "<p>" + formtitle.value + "</p>";
+        giftinfo += "<p>" + formdescription.value + "</p>";
         giftinfo += "</div>";
         var div = document.createElement("div");
         div.className = "gift";
         div.innerHTML = giftinfo;
         document.body.appendChild(div);
+        div.style = "font-family: Caviar Dreams; font-size: 1.5em; color: white; background: #E0003F; text-align: justify;";
         list = giftinfo.push;
         giftsArray.push(giftinfo);
         alert(giftsArray);
@@ -63,8 +65,15 @@ window.onload = function () {
             return ranNum;
           }
         var sitename = Current[0] + "a" + Current[1] + "b" + Current[2] + "c"; 
-        var link = "www.kidgifter.pl/" + sitename;
+        var link = "www.kidgifter.pl/test/" + sitename;
         var ended = true;
+        var go = document.createElement("a");
+        go.innerHTML = "Dalej";
+        go.className = "go";
+        go.href = sitename + ".php";
+        endlink.className = "endlink";
+        endlink.href = sitename + ".php";
+        document.body.appendChild(go);
         var allGifts = "";
         for($i = 0; i<=giftsArray.length; i++) {
             allGifts += giftsArray[i];    
